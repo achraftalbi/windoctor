85c9c894a274f0605cceac8246a7a7bdfdb9ef67
mobo <boufnichel.mohamed@gmail.com>
2015-09-28 16:13:48 +0100

    fix security bug

--- src/main/java/com/winbit/windoctor/config/SecurityConfiguration.java
+++ src/main/java/com/winbit/windoctor/config/SecurityConfiguration.java
@@ -118,6 +118,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/logs/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/audits/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/**").authenticated()
+            .antMatchers("/api/patients/**").hasAuthority(AuthoritiesConstants.USER)
             .antMatchers("/metrics/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/health/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/trace/**").hasAuthority(AuthoritiesConstants.ADMIN)
